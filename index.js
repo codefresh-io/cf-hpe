caf0cb5652f1ea4dec92a57f320128e2037caec7
Lior Shalev <liorshalev01@gmail.com>
2016-04-19 14:20:09 +0300

    updates

--- index.js
+++ index.js
@@ -1,26 +1,4 @@
 'use strict';
-import _ from 'lodash';
-import Rx from 'rx';
-import Request from 'request';
-
-class HpeError extends Error {
-  constructor(message) {
-    super(message);
-  }
-}
-
-class Hpe {
-  static authenticate() {
-    return Rx.Observable.throw(new HpeError("failed"));
-  }
-
-  static createCIServer() {
-    return Rx.Observable.throw(new HpeError("failed"));
-  }
-
-  static createPipeLine() {
-    return Rx.Observable.throw(new HpeError("failed"));
-  }
-}
+import Hpe from './lib/hpe';
 
 export default Hpe;
--- test/hpe.unit.spec.js
+++ test/hpe.unit.spec.js
@@ -3,6 +3,12 @@ import Hpe from '../index';
 
 describe('Authenticate with HPE', function () {
   it('Should return success for ', function (done) {
-    Hpe.authenticate().subscribe();
+    Hpe
+      .authenticate()
+      .subscribe(
+        result => {
+          done();
+        },
+        error => done(error));
   });
 });
