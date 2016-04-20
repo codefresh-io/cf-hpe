9a407ec56fd483b5e7f273ae1604cc1ebacbb9d6
Lior Shalev <liorshalev01@gmail.com>
2016-04-20 21:02:30 +0300

    updates

--- test/hpe.unit.spec.js
+++ test/hpe.unit.spec.js
@@ -8,34 +8,34 @@ import Hpe from '../index';
 const expect = Chai.expect;
 
 describe('HPE API Integration', function () {
-    it('Should return success for authentication', function (done) {
-        Hpe
-            .session()
-            .subscribe(session => {
-                    expect(session).to.have.property('request');
-                    done();
-                },
-                error => done(error));
-    });
+  it('Should return success for authentication', function (done) {
+    Hpe
+      .session()
+      .subscribe(session => {
+          expect(session).to.have.property('request');
+          done();
+        },
+        error => done(error));
+  });
 
-    it.only('Should return success for create server', function (done) {
-        const request = {
-            name: Util.format("ci-server-%d", _.now()),
-            instance_id: Uuid.v1()
-        };
+  it.only('Should return success for create server', function (done) {
+    const request = {
+      name: Util.format("ci-server-%d", _.now()),
+      instance_id: Uuid.v1()
+    };
 
-        Hpe
-            .session()
-            .flatMap(session => {
-                return Hpe.createServer(session, request);
-            })
-            .subscribe(response => {
-                    expect(response.name).to.equal(request.name);
-                    expect(response.instance_id).to.equal(request.instance_id);
-                    expect(response.server_type).to.equal('CodeFresh');
-                    done();
-                },
+    Hpe
+      .session()
+      .flatMap(session => {
+        return Hpe.createServer(session, request);
+      })
+      .subscribe(response => {
+          expect(response.name).to.equal(request.name);
+          expect(response.instance_id).to.equal(request.instance_id);
+          expect(response.server_type).to.equal('CodeFresh');
+          done();
+        },
 
-                error => done(error));
-    });
+        error => done(error));
+  });
 });
