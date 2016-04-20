9024742abc350f9184c23d5f5191a8e3d9414e69
Lior Shalev <liorshalev01@gmail.com>
2016-04-20 16:58:51 +0300

    create ci server

--- test/hpe.unit.spec.js
+++ test/hpe.unit.spec.js
@@ -1,12 +1,34 @@
 'use strict';
+import chai from 'chai';
 import Hpe from '../index';
 
-describe('Authenticate with HPE', function () {
-  it('Should return success for ', function (done) {
+const expect = chai.expect;
+
+describe('HPE API Integration', function () {
+  it('Should return success for authentication', function (done) {
     Hpe
       .session()
-      .subscribe(
-        result => {
+      .subscribe(session => {
+          expect(session).to.be.a('object');
+          expect(session).to.have.property('jar');
+
+          done();
+        },
+        error => done(error));
+  });
+
+  it('Should return success create server', function (done) {
+    Hpe
+      .session()
+      .flatMap(session => {
+        const data = {
+
+        };
+        Hpe.createServer(session, data);
+      }
+          expect(session).to.be.a('object');
+          expect(session).to.have.property('jar');
+
           done();
         },
         error => done(error));
