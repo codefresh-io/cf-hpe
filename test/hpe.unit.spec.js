4409d47a5a02101b2f03ed7cc817bb9ffc5150a7
Lior Shalev <liorshalev01@gmail.com>
2016-04-25 11:29:17 +0300

    test create pipeline

--- test/hpe.unit.spec.js
+++ test/hpe.unit.spec.js
@@ -38,4 +38,43 @@ describe('HPE API Integration', function () {
 
         error => done(error));
   });
+
+  it('Create pipeline should success', function (done) {
+
+    const request = {
+      "data": [
+        {
+          "name": "pipeline-01",
+          "ci_server": {
+            "id": 1001,
+            "type": "CodeFresh"
+          },
+          "root_job_ci_id": "job-id-01",
+          "jobs": [
+            {
+              "jobCiId": "job-id-01",
+              "name": "angular",
+              "parameters": []
+            }
+          ],
+          "releases": {
+            "id": 0,
+            "type": "release"
+          },
+        }
+      ]
+    };
+
+    Hpe
+      .session()
+      .flatMap(session => {
+        return Hpe.createPipeline(session, request);
+      })
+      .subscribe(response => {
+        expect(response.name).to.equal(request.name);
+        expect(response.instance_id).to.equal(request.instance_id);
+        expect(response.server_type).to.equal('CodeFresh');
+        done();
+      });
+  });
 });
