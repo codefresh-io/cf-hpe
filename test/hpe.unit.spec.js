a3f4289869be46abcd998ee165557b512c76f361
Lior Shalev <liorshalev01@gmail.com>
2016-04-25 10:16:27 +0300

    unit test

--- test/hpe.unit.spec.js
+++ test/hpe.unit.spec.js
@@ -18,10 +18,35 @@ describe('HPE API Integration', function () {
         error => done(error));
   });
 
-  it.only('Should return success for create server', function (done) {
+  it('Should return success for create server', function (done) {
     const request = {
-      name: Util.format("ci-server-%d", _.now()),
+      name: Util.format('ci-server-%d', _.now()),
       instance_id: Uuid.v1()
+    };
+
+    Hpe
+      .session()
+      .flatMap(session => {
+        return Hpe.createServer(session, request);
+      })
+      .subscribe(response => {
+          expect(response.name).to.equal(request.name);
+          expect(response.instance_id).to.equal(request.instance_id);
+          expect(response.server_type).to.equal('CodeFresh');
+          done();
+        },
+
+        error => done(error));
+  });
+
+  it('Should return success for create pipeline', function (done) {
+    const request = {
+      name: Util.format('pipeline-%d', _.now()),
+      ci_server: {
+        type: 'ci_server',
+        id: 1001
+      },
+      root_job_ci_id: ""
     };
 
     Hpe
