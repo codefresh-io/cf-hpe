667a1fc2f55fcaec32155ad4e07e26d7349e5d41
Lior Shalev <liorshalev01@gmail.com>
2016-04-27 16:21:40 +0300

    pipeline

--- test/hpe.unit.js
+++ test/hpe.unit.js
@@ -2,7 +2,8 @@
 import _ from 'lodash';
 import Util from 'util';
 import Chai from 'chai';
-import Hpe from '../index';
+import Hpe from '../lib/hpe';
+import HpePipeline from '../lib/hpe-pipeline';
 
 const expect = Chai.expect;
 
@@ -60,15 +61,17 @@ describe('Hpe', function () {
           expect(response.ci_server.id).to.equal(mock.serverID);
           expect(response.name).to.equal(pipeline.name);
 
-          const rootJobID = 'root-' + _.kebabCase(pipeline.name);
-          expect(response.root_job_ci_id).to.equal(rootJobID);
-          expect(response.jobs[0].jobCiId).to.equal(rootJobID);
-          expect(response.jobs[1].jobCiId).to.equal(rootJobID + '-clone-repository');
-          expect(response.jobs[2].jobCiId).to.equal(rootJobID + '-build-dockerfile');
-          expect(response.jobs[3].jobCiId).to.equal(rootJobID + '-unit-test-script');
-          expect(response.jobs[4].jobCiId).to.equal(rootJobID + '-push-docker-registry');
-          expect(response.jobs[5].jobCiId).to.equal(rootJobID + '-integration-test-script');
-          expect(response.jobs[6].jobCiId).to.equal(rootJobID + '-deploy-script');
+          const pipelineID = _.kebabCase(pipeline.name);
+          const pipelineJobs = HpePipeline.jobs(pipelineID);
+
+          expect(response.root_job_ci_id).to.equal(pipelineJobs[0].jobCiId);
+          expect(response.jobs[0].jobCiId).to.equal(pipelineJobs[0].jobCiId);
+          expect(response.jobs[1].jobCiId).to.equal(pipelineJobs[1].jobCiId);
+          expect(response.jobs[2].jobCiId).to.equal(pipelineJobs[2].jobCiId);
+          expect(response.jobs[3].jobCiId).to.equal(pipelineJobs[3].jobCiId);
+          expect(response.jobs[4].jobCiId).to.equal(pipelineJobs[4].jobCiId);
+          expect(response.jobs[5].jobCiId).to.equal(pipelineJobs[5].jobCiId);
+          expect(response.jobs[6].jobCiId).to.equal(pipelineJobs[6].jobCiId);
 
           mock.rootJobID = response.root_job_ci_id;
           done();
