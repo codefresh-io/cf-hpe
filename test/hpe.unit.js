4aeca954d573eb6855fb68a69960f6e444e99e0e
Lior Shalev <liorshalev01@users.noreply.github.com>
2016-04-27 16:58:02 +0300

    Cf 1770 (#2)
    * updates
    
    * hpe tests
    
    * hpe test
    
    * fix auth body
    
    * hpe tests
    
    * hpe tests
    
    * hpe test
    
    * use cookie jar
    
    * create ci server
    
    * create ci server
    
    * updates
    
    * update
    
    * updates
    
    * updates
    
    * extract csrf cookie
    
    * create ci server
    
    * create ci server
    
    * create ci server tests
    
    * updatea
    
    * updates
    
    * unit test
    
    * create pipeline
    
    * test create pipeline
    
    * create pipeline
    
    * error messages
    
    * create pipeline test
    
    * create pipeline jobs
    
    * Cf 1770 (#1)
    
    Cf 1770 As a developer i can report pipeline(build) statuses to HP NGA
    
    * create pipeline build
    
    * unit test updates
    
    * unit test
    
    * create pipeline updates
    
    * unit tests
    
    * unit tests
    
    * report build status
    
    * create pipeline build
    
    * updates

---
+++ test/hpe.unit.js
@@ -0,0 +1,112 @@
+'use strict';
+import _ from 'lodash';
+import Util from 'util';
+import Chai from 'chai';
+import Hpe from '../index';
+
+const expect = Chai.expect;
+
+describe('Hpe', function () {
+  this.timeout(5000);
+  const mock = {
+    session: undefined,
+    serverID: undefined,
+    serverInstanceID: undefined,
+    rootJobID: undefined
+  };
+
+  it('1-session', done => {
+    Hpe
+      .session()
+      .subscribe(function(session) {
+          expect(session).to.have.property('request');
+          mock.session = session;
+          done();
+        },
+        error => done(error));
+  });
+
+  it('2-create-server', done => {
+    const server = {
+      name: Util.format('Codefresh %d', _.now())
+    };
+
+    Hpe
+      .createServer(mock.session, server)
+      .subscribe(function (response) {
+          expect(response.id).to.be.a('number');
+          expect(response.name).to.equal(server.name);
+          expect(response.instance_id).to.equal(_.kebabCase(server.name));
+          expect(response.server_type).to.equal('Codefresh');
+
+          mock.serverID = response.id;
+          mock.serverInstanceID = response.instance_id;
+          done();
+        },
+        error => done(error));
+  });
+
+  it('3-create-pipeline', function (done) {
+    const pipeline = {
+      serverID: mock.serverID,
+      name: Util.format('Codefresh %d', _.now()),
+    };
+
+    Hpe
+      .createPipeline(mock.session, pipeline)
+      .subscribe(function(response) {
+          expect(response.id).to.be.a('number');
+          expect(response.root_job.id).to.be.a('number');
+          expect(response.ci_server.id).to.equal(mock.serverID);
+          expect(response.name).to.equal(pipeline.name);
+
+          const rootJobID = 'root-' + _.kebabCase(pipeline.name);
+          expect(response.root_job_ci_id).to.equal(rootJobID);
+          expect(response.jobs[0].jobCiId).to.equal(rootJobID);
+          expect(response.jobs[1].jobCiId).to.equal(rootJobID + '-clone-repository');
+          expect(response.jobs[2].jobCiId).to.equal(rootJobID + '-build-dockerfile');
+          expect(response.jobs[3].jobCiId).to.equal(rootJobID + '-unit-test-script');
+          expect(response.jobs[4].jobCiId).to.equal(rootJobID + '-push-docker-registry');
+          expect(response.jobs[5].jobCiId).to.equal(rootJobID + '-integration-test-script');
+          expect(response.jobs[6].jobCiId).to.equal(rootJobID + '-deploy-script');
+
+          mock.rootJobID = response.root_job_ci_id;
+          done();
+        },
+        error => done(error));
+  });
+
+  it('4-start-pipeline-build', done => {
+    const build = {
+      serverID: mock.serverID,
+      jobID: mock.rootJobID
+    };
+
+    Hpe
+      .startPipelineBuild(mock.session, build)
+      .subscribe(response => {
+
+          done();
+        },
+        error => done(error));
+  });
+
+  it.skip('5-report-pipeline-build', done => {
+    const build = {
+      serverID: mock.serverInstanceID,
+      jobID: mock.rootJobID,
+      startTime: _.now(),
+      duration: 1000,
+      status: 'finished',
+      result: 'success',
+    };
+
+    Hpe
+      .reportPipelineBuildStatus(mock.session, build)
+      .subscribe(response => {
+
+          done();
+        },
+        error => done(error));
+  });
+});
--- test/hpe.unit.spec.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-import Hpe from '../index';
-
-describe('Authenticate with HPE', function () {
-  it('Should return success for ', function (done) {
-    Hpe.authenticate().subscribe();
-  });
-});
