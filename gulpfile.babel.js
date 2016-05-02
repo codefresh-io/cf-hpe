d6976ece6d910eeeadbeb26c8eadf1666bb89777
Lior Shalev <liorshalev01@gmail.com>
2016-05-02 20:42:42 +0300

    gulp

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,17 +3,17 @@ import eslint from 'gulp-eslint';
 import mocha from 'gulp-mocha';
 import processEnv from 'gulp-process-env';
 
-gulp.task('default', ['lint'], () => {
+gulp.task('default', ['lint', 'test'], () => {
 
 });
+
+gulp.task('test', ['unit-test', 'integration-test']);
 
 gulp.task('lint', () =>
   gulp.src(['**/*.js', '!node_modules/**'])
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError()));
-
-gulp.task('test', ['lint', 'unit-test', 'integration-test']);
 
 gulp.task('unit-test', () =>
   gulp.src(['test/unit-*.js'], { read: false })
@@ -24,7 +24,7 @@ gulp.task('integration-test', () =>
     .pipe(processEnv({
       HPE_SERVER_URL: 'http://146.148.93.246:8080',
       HPE_USER: 'cf_p32zor9g9gl2zbp0rp663oex0',
-      HPE_PASSWORD: '=211cb1cdb045df37I',
+      HPE_PASSWORD: '==211cb1cdb045df37I',
       HPE_SHARED_SPACE: '1001',
       HPE_WORKSPACE: '1002',
     }))
