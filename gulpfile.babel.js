1a43bcbf5ce43d7e3212563e00044d46915d8c1b
Lior Shalev <liorshalev01@gmail.com>
2016-05-03 19:39:16 +0300

    build

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,11 +1,8 @@
 import gulp from 'gulp';
 import eslint from 'gulp-eslint';
+import babel from 'gulp-babel';
 import mocha from 'gulp-mocha';
 import processEnv from 'gulp-process-env';
-
-gulp.task('default', ['test']);
-
-gulp.task('test', ['unit-test']);
 
 gulp.task('lint', () =>
   gulp.src(['**/*.js', '!node_modules/**'])
@@ -13,16 +10,21 @@ gulp.task('lint', () =>
     .pipe(eslint.format())
     .pipe(eslint.failAfterError()));
 
-gulp.task('unit-test', ['lint'], () =>
-  gulp.src(['test/unit-*.js'], { read: false })
+gulp.task('build', ['lint'], () =>
+  gulp.src(['**/*.js', '!node_modules/**'])
+    .pipe(babel())
+    .pipe(gulp.dest('build')));
+
+gulp.task('test', ['build'], () =>
+  gulp.src(['build/test/unit-*.js'], { read: false })
     .pipe(mocha({ reporter: 'spec' })));
 
-gulp.task('integration-test', ['unit-test'], () =>
-  gulp.src(['test/integration-*.js'], { read: false })
+gulp.task('integration-test', ['test'], () =>
+  gulp.src(['build/test/integration-*.js'], { read: false })
     .pipe(processEnv({
       HPE_SERVER_URL: 'http://146.148.93.246:8080',
       HPE_USER: 'cf_p32zor9g9gl2zbp0rp663oex0',
-      HPE_PASSWORD: '=211cb1cdb045df37I',
+      HPE_PASSWORD: '==211cb1cdb045df37I',
       HPE_SHARED_SPACE: '1001',
       HPE_WORKSPACE: '1002',
     }))
