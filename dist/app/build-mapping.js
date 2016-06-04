3fca32372ac3acba8d839c9014be0a7662e23bf9
Lior Shalev <liorshalev01@users.noreply.github.com>
2016-06-04 19:13:34 +0300

    Work progress (#33)
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * hope mocha reporter
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * test results
    
    * updates
    
    * fix duplicate step status
    
    * updates
    
    * Release build
    
    * Release build
    
    * Release build
    
    * Release build
    
    * Release build
    
    * Release build
    
    * updates
    
    * updates
    
    * updates
    
    * release
    
    * updates
    
    * updates
    
    * fix build id and name
    
    User build._id and build.commit for build id and name
    
    * updates
    
    * release
    
    * updates
    
    * release
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * release
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * updates
    
    * allow all accounts (isHpeIntegrationAccount)
    
    * build steps
    
    * updates
    
    * release

--- dist/app/build-mapping.js
+++ dist/app/build-mapping.js
@@ -24,7 +24,8 @@ HpeStatusMapping.isStatus = function (status) {
 var HpePipelineStepMapping = exports.HpePipelineStepMapping = {
   'Building Docker Image': 'build-dockerfile',
   'Running Unit Tests': 'unit-test-script',
-  'Running Integration Tests': 'security-validation',
+  'Running Integration Tests': 'integration-test-script',
+  'security-validation': 'security-validation',
   'Running Deploy script': 'deploy-script'
 };
 
