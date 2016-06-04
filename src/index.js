68051f037230f0f427a613f7b5453e8c0c328007
Lior Shalev <liorshalev01@gmail.com>
2016-06-04 14:51:01 +0300

    reporters

--- src/index.js
+++ src/index.js
@@ -1,63 +1,15 @@
 import R from 'ramda';
+import { HpeApiTestResult } from 'cf-hpe-api';
 import { Build } from 'app/build';
 import { BuildStep } from 'app/build-step';
 import { BuildSession } from 'app/build-session';
-import { HpeApiTestResult } from 'cf-hpe-api';
-
-const aquaResults = require('./aqua.json');
-
-const hpeTestResultMapping = {
-  pass: 'Passed',
-  fail: 'Failed',
-};
-
-const reportBuildPipelineSteps = (buildStepObservable, buildSession) =>
-  buildStepObservable.flatMap(step => BuildSession.reportBuildPipelineStepStatus(
-    buildSession,
-    step));
-
-const reportBuildPipelineTests = (buildStepObservable, buildSession) =>
-  buildStepObservable
-    .filter(step => R.contains(step.stepId, ['unit-test-script']))
-    .flatMap(step => BuildStep
-      .childStepLogs(step)
-      .filter(R.test(/^\["(pass|fail)",{"title":.+}]\s+$/))
-      .map(JSON.parse)
-      .map(testResult => HpeApiTestResult.create(
-        testResult[1].fullTitle,
-        step.startTime,
-        testResult[1].duration,
-        hpeTestResultMapping[testResult[0]],
-        testResult[1].err,
-        testResult[1].err,
-        testResult[1].stack))
-      .flatMap(hpeApiTestResult => BuildSession.reportBuildPipelineTestResults(
-        buildSession,
-        step,
-        [hpeApiTestResult])));
-
-const reportBuildPipelineSecurityTests = (buildStepObservable, buildSession) =>
-  buildStepObservable
-    .filter(step => R.contains(step.stepId, ['security-validation']))
-    .flatMap(() => aquaResults)
-      .map(JSON.parse)
-      .map(testResult => HpeApiTestResult.create(
-        testResult[1].fullTitle,
-        step.startTime,
-        testResult[1].duration,
-        hpeTestResultMapping[testResult[0]],
-        testResult[1].err,
-        testResult[1].err,
-        testResult[1].stack))
-      .flatMap(hpeApiTestResult => BuildSession.reportBuildPipelineTestResults(
-        buildSession,
-        step,
-        [hpeApiTestResult]));
+import { CommonPipelineReporter } from 'app/reporters/common-pipeline-reporter';
+import { MochaJsonStreamReporter } from 'app/reporters/mocha-json-stream-reporter';
 
 Build.buildsFromFirebase().flatMap(build =>
   BuildSession.createForBuild(build).map(buildSession => {
     const buildStepObservable = BuildStep.stepsFromBuild(build).share();
-    reportBuildPipelineSteps(buildStepObservable, buildSession).subscribe();
-    reportBuildPipelineTests(buildStepObservable, buildSession).subscribe();
+    CommonPipelineReporter.create(buildStepObservable, buildSession).subscribe();
+    MochaJsonStreamReporter.create(buildStepObservable, buildSession).subscribe();
     return {};
   })).subscribe();
